c22084f0a96a908f03fbcd1f1d9fed0ec2d6d464
steve <hfaghih80@gmail.com>
2022-05-10 18:40:27 +0430

    named pipe added and few changes

--- main.c
+++ main.c
@@ -3,89 +3,129 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+int n,m,p,fd,status;
+char * myfifo = "myfifo";
 
 int main (){
+    char input[1000000]=" ";
+    FILE* ptr;
+    char ch;
+    mkfifo(myfifo, 0666);
+    ptr = fopen("testcase1.txt","r");
+    do {
+        ch = fgetc(ptr);
+        strncat(input, &ch, 1);
+    } while (ch != EOF);
+    //printf("%s",input);
+    int len = sizeof(input)/sizeof(input[0]);
+    int reallen = 0;
+    int i,j;
+    for ( i=0;i<len;i++){
+        int x = input[i];
+        if (!isspace(input[i]) && x != 0){
+            reallen = (i+1);
+        }
+    }
+    //printf("%d",reallen);
+    //printf ("%d",len);    
+    for(i = 0; i < reallen; i++){  
+        if(isspace(input[i])){  
+            for(j=i;j<reallen;j++){  
+                input[j]=input[j+1];  
+            }  
+            reallen--;
+            i--;  
+        }  
+    }
+    //printf ("%ld",strlen(input));
+    int xx = strlen(input);
+    int cnt = 1;
+    char ch1[4] = "",ch2[4] = "",ch3[4] = "";
+    for (i = 0;i<xx;i++){
+        if (input[i] == '*'){
+            cnt++;
+        }
+        else if (cnt == 1){
+            strncat(ch1,&input[i],1);
+        }
+        else if (cnt == 2){
+            strncat(ch2,&input[i],1);
+        }
+        else if (cnt == 3 && isdigit(input[i])){
+            strncat(ch3,&input[i],1);
+        }
+        else {
+            break;
+        }
+    }
+    n = atoi(ch1); m = atoi (ch2) ; p = atoi (ch3) ;
+    //printf ("%d  %d  %d " , n , m ,p);
+        
     int pid1 = fork();
     if (pid1 == 0){
-        int n,m,p;
-        char input[1000000]=" ";
-        FILE* ptr;
-        char ch;
-        ptr = fopen("testcase1.txt","r");
-        do {
-            ch = fgetc(ptr);
-            strncat(input, &ch, 1);
-        } while (ch != EOF);
-        //printf("%s",input);
-        int len = sizeof(input)/sizeof(input[0]);
-        int reallen = 0;
-        int i,j;
-        for ( i=0;i<len;i++){
-            int x = input[i];
-            if (!isspace(input[i]) && x != 0){
-                reallen = (i+1);
-            }
-        }
-        //printf("%d",reallen);
-        //printf ("%d",len);    
-        for(i = 0; i < reallen; i++){  
-            if(isspace(input[i])){  
-                for(j=i;j<reallen;j++){  
-                    input[j]=input[j+1];  
-                }  
-                reallen--;
-                i--;  
-            }  
-        }
-        //printf ("%ld",strlen(input));
-        int xx = strlen(input);
-        int cnt = 1;
-        char ch1[4] = "",ch2[4] = "",ch3[4] = "";
-        for (i = 0;i<xx;i++){
-            if (input[i] == '*'){
-                cnt++;
-            }
-            else if (cnt == 1){
-                strncat(ch1,&input[i],1);
-            }
-            else if (cnt == 2){
-                strncat(ch2,&input[i],1);
-            }
-            else if (cnt == 3 && isdigit(input[i])){
-                strncat(ch3,&input[i],1);
-            }
-            else {
-                break;
-            }
-        }
-        n = atoi(ch1); m = atoi (ch2) ; p = atoi (ch3) ;
-        //printf ("%d  %d  %d " , n , m ,p);
-        char table [n+1][n+1];
-        int x = 1 , y = 1;
-        for (i=0;i<xx;i++){
-            if (isalpha(input[i])){
-                table[x][y] = input[i];
-                if (y == n){
-                    x++;
-                    y = 1;
-                }
-                else{
-                    y++;
+        int pid2 = fork();
+        if (pid2 == 0){
+            char table [n+1][n+1];
+            int x = 1 , y = 1;
+            for (i=0;i<xx;i++){
+                if (isalpha(input[i])){
+                    table[x][y] = input[i];
+                    if (y == n){
+                        x++;
+                        y = 1;
+                    }
+                    else{
+                        y++;
+                    }
                 }
             }
+            // for (i=1;i<=n;i++){
+            //     for (j=1;j<=n;j++){
+            //         printf ("%c",table[i][j]);
+            //     }
+            //     printf ("\n");
+            // }
+            // printf ("===========\n");
+            for (i=1;i<=n;i++){
+                for (j=1;j<=n;j++){
+                    table[i][j] = toupper(table[i][j]);
+                    if (  (table[i][j] >= 'A' && table[i][j] <= 'X' )) {
+                        table[i][j] +=2;
+                    }
+                    else if (table[i][j] == 'Y'){
+                        table[i][j] = 'A';
+                    }
+                    else if (table[i][j] == 'Z'){
+                        table[i][j] = 'B';
+                    }
+                    printf ("%c",table[i][j]);
+                }
+                printf ("\n");
+            }
+            // printf ("1===========\n");
+            fd = open(myfifo, O_WRONLY);
+            write(fd,table,(n+1)*(n+1));
+        }
+        else if (pid2 != 0){
+            waitpid(pid2,&status,0);
+            fd = open(myfifo,O_RDONLY);
+            char table[n+1][n+1];
+            read(fd, table, (n+1)*(n+1));
+            // for (i=1;i<=n;i++){
+            //     for (j=1;j<=n;j++){
+            //         printf ("%c",table[i][j]);
+            //     }
+            //     printf ("\n");
+            // }
+            // printf ("2===========\n");
         }
         
-        for (i=1;i<=n;i++){
-            for (j=1;j<=n;j++){
-                printf ("%c",table[i][j]);
-            }
-            printf ("\n");
-        }
     }
     
 }
