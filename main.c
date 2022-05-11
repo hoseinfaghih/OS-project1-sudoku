9f1977f40d141be3ca6f398d15de05417345ddec
steve <hfaghih80@gmail.com>
2022-05-11 19:29:21 +0430

    final commit

--- main.c
+++ main.c
@@ -9,14 +9,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int n,m,p,fd,status;
+int n,m,p,fd,fd1,fd2,fd3,fd4,status,status1;
 char * myfifo = "myfifo";
+char * myfifo1 = "myfifo1";
+char * myfifo2 = "myfifo2";
+char * myfifo3 = "myfifo3";
+char * myfifo4 = "myfifo4";
 
 int main (){
     char input[1000000]=" ";
     FILE* ptr;
     char ch;
     mkfifo(myfifo, 0666);
+    mkfifo(myfifo1,0666);
+    mkfifo(myfifo2,0666);
+    mkfifo(myfifo3,0666);
+    mkfifo(myfifo4,0666);
     ptr = fopen("testcase1.txt","r");
     do {
         ch = fgetc(ptr);
@@ -32,18 +40,15 @@ int main (){
             reallen = (i+1);
         }
     }
-    //printf("%d",reallen);
-    //printf ("%d",len);    
-    for(i = 0; i < reallen; i++){  
-        if(isspace(input[i])){  
-            for(j=i;j<reallen;j++){  
-                input[j]=input[j+1];  
-            }  
+    for(i = 0; i < reallen; i++){
+        if(isspace(input[i])){
+            for(j=i;j<reallen;j++){
+                input[j]=input[j+1];
+            }
             reallen--;
-            i--;  
-        }  
+            i--;
+        }
     }
-    //printf ("%ld",strlen(input));
     int xx = strlen(input);
     int cnt = 1;
     char ch1[4] = "",ch2[4] = "",ch3[4] = "";
@@ -65,104 +70,198 @@ int main (){
         }
     }
     n = atoi(ch1); m = atoi (ch2) ; p = atoi (ch3) ;
-    //printf ("%d  %d  %d " , n , m ,p);
-        
+
     int pid1 = fork();
-    if (pid1 == 0){
-        int pid2 = fork();
-        if (pid2 == 0){
-            char table [n+1][n+1];
-            int x = 1 , y = 1;
-            for (i=0;i<xx;i++){
-                if (isalpha(input[i])){
-                    table[x][y] = input[i];
-                    if (y == n){
-                        x++;
-                        y = 1;
-                    }
-                    else{
-                        y++;
-                    }
+    int pid2 = fork();
+    if (pid1 > 0 && pid2 > 0){
+        char table [n+1][n+1];
+        int x = 1 , y = 1;
+        for (i=0;i<xx;i++){
+            if (isalpha(input[i])){
+                table[x][y] = input[i];
+                if (y == n){
+                    x++;
+                    y = 1;
+                }
+                else{
+                       y++;
                 }
             }
-            // for (i=1;i<=n;i++){
-            //     for (j=1;j<=n;j++){
-            //         printf ("%c",table[i][j]);
-            //     }
-            //     printf ("\n");
-            // }
-            // printf ("===========\n");
-            for (i=1;i<=n;i++){
-                for (j=1;j<=n;j++){
-                    table[i][j] = toupper(table[i][j]);
-                    if (  (table[i][j] >= 'A' && table[i][j] <= 'X' )) {
-                        table[i][j] +=2;
-                    }
-                    else if (table[i][j] == 'Y'){
-                        table[i][j] = 'A';
-                    }
-                    else if (table[i][j] == 'Z'){
-                        table[i][j] = 'B';
-                    }
-                    printf ("%c",table[i][j]);
+        }
+
+        for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                table[i][j] = toupper(table[i][j]);
+                if (  (table[i][j] >= 'A' && table[i][j] <= 'X' )) {
+                    table[i][j] +=2;
                 }
-                printf ("\n");
+                else if (table[i][j] == 'Y'){
+                    table[i][j] = 'A';
+                }
+                else if (table[i][j] == 'Z'){
+                    table[i][j] = 'B';
+                }
+                //printf ("%c",table[i][j]);
             }
-            // printf ("1===========\n");
-            fd = open(myfifo, O_WRONLY);
-            write(fd,table,(n+1)*(n+1));
+            //printf ("\n");
         }
-        else if (pid2 != 0){
-            waitpid(pid2,&status,0);
-            fd = open(myfifo,O_RDONLY);
-            char table[n+1][n+1];
-            read(fd, table, (n+1)*(n+1));
-            // for (i=1;i<=n;i++){
-            //     for (j=1;j<=n;j++){
-            //         printf ("%c",table[i][j]);
-            //     }
-            //     printf ("\n");
-            // }
-            // printf ("2===========\n");
-        }
-        
+        //printf ("1===========\n");
+        fd = open(myfifo, O_WRONLY);
+        write(fd,table,(n+1)*(n+1));
+        close(fd);
+        fd1 = open(myfifo1, O_WRONLY);
+        write(fd1,table,(n+1)*(n+1));
+        close(fd1);
+        fd2 = open(myfifo2, O_WRONLY);
+        write(fd2,table,(n+1)*(n+1));
+        close(fd2);
+        exit(0);
+
     }
-    
+    else if (pid1 == 0 && pid2 > 0){
+        sleep(1);
+        fd = open(myfifo,O_RDONLY);
+        char table[n+1][n+1];
+        read(fd, table, (n+1)*(n+1));
+        close(fd);
+        int mark[26] = {0};
+        int flag = 1;
+        /*for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                printf ("%c",table[i][j]);
+            }
+            printf ("\n");
+        }
+        printf ("2===========\n");*/
+        for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                int tmp = table[i][j]  - 'A';
+                mark[tmp] += 1;
+                //printf ("%d  %d\n",mark[tmp],tmp);
+                if (mark[tmp] != i){
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0){
+                break;
+            }
+        }
+        //printf ("%d\n" ,flag);
+        char wr[] = "0";
+        if (flag == 1){
+            wr[0] = '1';
+        }
+        fd3 = open(myfifo3, O_WRONLY);
+        write(fd3,wr,1);
+        close(fd3);
+        //printf ("Yay");
+        exit(0);
+
+    }
+    else if (pid1 > 0 && pid2 == 0){
+        sleep(2);
+        fd1 = open(myfifo1,O_RDONLY);
+        char table[n+1][n+1];
+        read(fd1, table, (n+1)*(n+1));
+        close(fd1);
+        int mark[26] = {0};
+        int flag = 1;
+        /*for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                printf ("%c",table[i][j]);
+            }
+            printf ("\n");
+        }
+        printf ("3===========\n");*/
+        for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                int tmp = table[j][i]  - 'A';
+                mark[tmp] += 1;
+                //printf ("%d  %d\n",mark[tmp],tmp);
+                if (mark[tmp] != i){
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0){
+                break;
+            }
+        }
+        //printf ("%d\n" ,flag);
+
+        char wr[] = "0";
+        if (flag == 1){
+            wr[0] = '1';
+        }
+
+        fd4 = open(myfifo4, O_WRONLY);
+        write(fd4,wr,1);
+        close(fd4);
+        //printf ("Yay");
+        exit(0);
+    }
+    else if (pid2 == 0 && pid1 == 0){
+        sleep(3);
+        fd2 = open(myfifo2,O_RDONLY);
+        char table[n+1][n+1];
+        read(fd2, table, (n+1)*(n+1));
+        close(fd2);
+        int mark[26] = {0};
+        int flag = 1;
+        /*for (i=1;i<=n;i++){
+            for (j=1;j<=n;j++){
+                printf ("%c",table[i][j]);
+            }
+            printf ("\n");
+        }
+        printf ("4===========\n");*/
+        int x = n / m;
+        int y = n / p ;
+        //printf("%d %d %d %d %d" , n , m , p , x , y);
+        int cnt = 0 ;
+        for (i=1;i<=x;i++){
+            for (j=1;j<=y;j++){
+                cnt += 1;
+                int indx = 1 , indy = 1;
+                if (i > 1){
+                    indy += (i-1)*m;
+                }
+                if (j > 1){
+                    indx += (j-1)*p;
+                }
+
+                int ii,jj;
+                for (ii=indx;ii<indx+p;ii++){
+                    for (jj=indy;jj<indy+m;jj++){
+                        int tmp = table[ii][jj]  - 'A';
+                        mark[tmp] += 1;
+                        //printf ("%d  %d %d %d  %c  %d  %d\n",i,j,ii,jj,table[ii][jj],mark[tmp],tmp);
+                        if (mark[tmp] != cnt){
+                            flag = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //printf ("%d" ,flag);
+        fd3 = open (myfifo3 , O_RDONLY);
+        char flag2[1],flag3[1];
+        read(fd3,flag2,1);
+        close(fd3);
+        fd4 = open (myfifo4 , O_RDONLY);
+        read(fd4,flag3,1);
+        close(fd4);
+        //printf ("%c  %c" , flag2[0], flag3[0]);
+        if (flag == 1 && flag2[0] == '1' && flag3[0] == '1'){
+            printf("‫‪Sudoku‬‬ ‫‪Puzzle‬‬ ‫‪constraints‬‬ ‫‪satisfied‬‬\n");
+        }
+        else{
+            printf("‫‪Sudoku‬‬ ‫‪Puzzle‬‬ ‫‪is‬‬ ‫‪Wrong‬‬");
+        }
+    }
+
 }
 
-// int fd;
 
-//     // FIFO file path
-//     char * myfifo = "/tmp/myfifo";
-
-//     // Creating the named file(FIFO)
-//     // mkfifo(<pathname>, <permission>)
-//     mkfifo(myfifo, 0666)
-
-// ;
-
-// char arr1[80], arr2[80];
-//     while (1)
-//     {
-//         // Open FIFO for write only
-//         fd = open(myfifo, O_WRONLY);
-
-//         // Take an input arr2ing from user.
-//         // 80 is maximum length
-//         fgets(arr2, 80, stdin);
-
-//         // Write the input arr2ing on FIFO
-//         // and close it
-//         write(fd, arr2, strlen(arr2)+1);
-//         close(fd);
-
-//         // Open FIFO for Read only
-//         fd = open(myfifo, O_RDONLY);
-
-//         // Read from FIFO
-//         read(fd, arr1, sizeof(arr1));
-
-//         // Print the read message
-//         printf("User2: %s\n", arr1);
-//         close(fd);
-//     }
